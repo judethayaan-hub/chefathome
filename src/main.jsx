cf9ba90183d19eee9f1e2e03837f344a9611e4fa
judethayaan-hub <judethayaan@gmail.com>
2026-06-13 12:10:03 +0530

    Update Evara task1

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../index.css";
+import App from "./Evara";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
+
+// Register service worker for PWA support
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
